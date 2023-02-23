6648096b2078aa2549ea40fd607779aea5f454ce
Javed <jdhn42@gmail.com>
2023-02-23 11:50:35 -0500

    refactor: update src/constants.ts
    Co-authored-by: Derek Sonnenberg <derek@ardrive.io>

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ const testEnvPort = 1235;
 const prodEnvPort = 3000;
 
 export const defaultPort = isTestEnv ? testEnvPort : prodEnvPort;
-export const oneMinute = 60 * 1000 * 1000;
+export const oneMinute = 1000 * 60 * 60;
