089a0e92ad6da8d3c5dee8ba2a12c232802f5a9f
Javed Hussein <jdhn42@gmail.com>
2023-02-27 12:18:33 -0500

    refactor: add winston and ar types PE-2997

--- src/pricing/pricing.ts
+++ src/pricing/pricing.ts
@@ -1,9 +1,11 @@
 import BigNumber from "bignumber.js";
 
+import { AR } from "../types/ar";
+import { Winston } from "../types/winston";
 import { ReadThroughBytesToArOracle } from "./oracles/bytesToAROracle";
 import { ReadThroughFiatToArOracle } from "./oracles/fiatToAROracle";
 
-type ARC = BigNumber;
+type ARC = Winston;
 
 export interface PricingService {
   getARCForFiat: (fiat: string) => Promise<ARC>;
@@ -24,11 +26,11 @@ export class TurboPricingService implements PricingService {
 
   async getARCForFiat(fiat: string): Promise<ARC> {
     const ar = await this.fiatToAROracle.getARForFiat(fiat);
-    return new BigNumber(ar);
+    return AR.from(BigNumber(ar)).toARC();
   }
 
   async getARCForBytes(bytes: number): Promise<ARC> {
     const ar = await this.bytesToAROracle.getARForBytes(bytes);
-    return new BigNumber(ar);
+    return AR.from(BigNumber(ar)).toARC();
   }
 }
