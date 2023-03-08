daac99130d957f1643ea3f70e80be793731697fc
Javed Hussein <jdhn42@gmail.com>
2023-03-07 21:37:21 -0500

    refactor: remove duplicate type PE-3023

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,5 @@
 export type Base64String = string;
 export type PublicArweaveAddress = Base64String;
 export type TransactionId = Base64String;
-export type ByteCount = number;
 
 export * from "./winston";
