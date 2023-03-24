05f655fbc8a60e92664c737c1030035b2af6eadc
Derek Sonnenberg <derek@ardrive.io>
2023-03-24 12:08:33 -0500

    test: setup test for createPaymentReceipt PE-3031

--- src/database/postgres.test.ts
+++ src/database/postgres.test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 
 import { DbTestHelper } from "../../tests/dbTestHelper";
 import { Winston } from "../types/winston";
-import { TopUpQuoteDBResult } from "./dbTypes";
+import { PaymentReceiptDBResult, TopUpQuoteDBResult } from "./dbTypes";
 import { PostgresDatabase } from "./postgres";
 
 /** Knex instance connected to a PostgreSQL database */
@@ -89,6 +89,66 @@ describe("PostgresDatabase class", () => {
 
       expect(pantsQuote.topUpQuoteId).to.equal(pantsId);
       expect(shortsQuote.topUpQuoteId).to.equal(shortsId);
+    });
+  });
+
+  describe("createPaymentReceipt method", () => {
+    const quoteExpirationDate = new Date(
+      "2023-03-23 12:34:56.789Z"
+    ).toISOString();
+
+    before(async () => {
+      // TODO: Before sending to DB and creating top up quote we should use safer types:
+      // -  validate this address is a public arweave address (and address type is arweave)
+      // -  validate payment provider is expected
+      // -  validate currency type is supported
+      await db.createPaymentReceipt({
+        amount: 10101,
+        currencyType: "can",
+        destinationAddress: "A Grand Address :)",
+        destinationAddressType: "arweave",
+        topUpQuoteId: "A New Top Up ID",
+        paymentProvider: "stripe",
+        paymentReceiptId: "Unique Identifier",
+        winstonCreditAmount: new Winston(
+          Number.MAX_SAFE_INTEGER.toString() + "00"
+        ),
+      });
+    });
+
+    after(async () => {
+      await dbTestHelper.cleanUpEntityInDb("top_up_quote", "Unique Identifier");
+    });
+
+    it("creates the expected top_up_quote in the database entity", async () => {
+      const paymentReceipt = await db["knex"]<PaymentReceiptDBResult>(
+        "top_up_quote"
+      ).where({ payment_receipt_id: "Unique Identifier" });
+      expect(paymentReceipt.length).to.equal(1);
+
+      const {
+        amount,
+        currency_type,
+        destination_address,
+        destination_address_type,
+        payment_provider,
+        payment_receipt_date,
+        payment_receipt_id,
+        top_up_quote_id,
+        winston_credit_amount,
+      } = paymentReceipt[0];
+
+      expect(amount).to.equal("10101");
+      expect(currency_type).to.equal("can");
+      expect(destination_address).to.equal("A Grand Address :)");
+      expect(destination_address_type).to.equal("arweave");
+      expect(payment_provider).to.equal("stripe");
+      expect(payment_receipt_date).to.exist;
+      expect(payment_receipt_id).to.equal("Unique Identifier");
+      expect(top_up_quote_id).to.equal("A New Top Up ID");
+      expect(winston_credit_amount).to.equal(
+        Number.MAX_SAFE_INTEGER.toString() + "00"
+      );
     });
   });
 
