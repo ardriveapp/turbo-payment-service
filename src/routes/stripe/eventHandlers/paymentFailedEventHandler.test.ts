7f5cfae7cbea79befbdc43845c08549936978b0d
Derek Sonnenberg <derek@ardrive.io>
2023-03-28 10:56:29 -0500

    test: adjust payment failed expectations PE-3031

--- src/routes/stripe/eventHandlers/paymentFailedEventHandler.test.ts
+++ src/routes/stripe/eventHandlers/paymentFailedEventHandler.test.ts
@@ -1,50 +1,73 @@
-import * as chai from "chai";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
+import { expect } from "chai";
 
 import { paymentIntentFailedStub } from "../../../../tests/helpers/stubs";
+import { tableNames } from "../../../database/dbConstants";
+import {
+  FailedTopUpQuoteDBResult,
+  PaymentReceiptDBResult,
+  TopUpQuoteDBResult,
+} from "../../../database/dbTypes";
+import { PostgresDatabase } from "../../../database/postgres";
 import { handlePaymentFailedEvent } from "./paymentFailedEventHandler";
 
-var expect = chai.expect;
-chai.use(sinonChai);
-
 describe("handlePaymentFailedEvent", () => {
-  let sandbox: sinon.SinonSandbox;
-  const mockDatabase = {
-    expireTopUpQuote: () => Promise.resolve({}),
-    createRefundReceipt: () => Promise.resolve({}),
-  };
-  const mockCtx = {
-    state: {
-      paymentDatabase: mockDatabase,
-    },
-  };
-  beforeEach(() => {
-    sandbox = sinon.createSandbox();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
+  const db = new PostgresDatabase();
 
   // TODO: Integrate with test db
   it.skip("should capture the payment failed event and create refund receipt", async () => {
     const paymentIntent = paymentIntentFailedStub;
 
-    const expirePriceQuoteStub = sandbox
-      .stub(mockDatabase, "expireTopUpQuote")
-      .resolves({ walletAddress: "", balance: 10 });
-    const createRefundReceiptStub = sandbox
-      .stub(mockDatabase, "createRefundReceipt")
-      .resolves({});
+    await handlePaymentFailedEvent(paymentIntent, db);
 
-    await handlePaymentFailedEvent(paymentIntent, mockCtx as any);
+    // Payment receipt was not created
+    const paymentReceiptDbResults = await db["knex"]<PaymentReceiptDBResult>(
+      tableNames.paymentReceipt
+    ).where({
+      payment_receipt_id: paymentIntent.id,
+    });
+    expect(paymentReceiptDbResults).to.have.length(0);
 
-    expect(expirePriceQuoteStub).to.have.been.calledWith(
-      paymentIntent.metadata["address"]
-    );
-    expect(createRefundReceiptStub).to.have.been.calledWith(
-      paymentIntent.metadata["address"]
-    );
+    // Top up quote was expired
+    const topUpQuoteDbResults = await db["knex"]<TopUpQuoteDBResult>(
+      tableNames.topUpQuote
+    ).where({
+      // TODO: where to get top up id
+      top_up_quote_id: paymentIntent.id,
+    });
+    expect(topUpQuoteDbResults).to.have.length(0);
+
+    // Top up quote was marked as failed
+    const failedTopUpQuoteDbResults = await db[
+      "knex"
+    ]<FailedTopUpQuoteDBResult>(tableNames.failedTopUpQuote).where({
+      // TODO: where to get top up id
+      top_up_quote_id: paymentIntent.id,
+    });
+    expect(failedTopUpQuoteDbResults).to.have.length(1);
+
+    const {
+      amount,
+      currency_type,
+      destination_address,
+      destination_address_type,
+      payment_provider,
+      quote_creation_date,
+      quote_expiration_date,
+      quote_failed_date,
+      top_up_quote_id,
+      winston_credit_amount,
+    } = failedTopUpQuoteDbResults[0];
+
+    // TODO: Answer ? below expectations
+    expect(amount).to.equal(paymentIntent.amount);
+    expect(currency_type).to.equal(paymentIntent.currency);
+    expect(destination_address).to.equal("?");
+    expect(destination_address_type).to.equal("arweave");
+    expect(payment_provider).to.equal("stripe");
+    expect(quote_failed_date).to.exist;
+    expect(quote_creation_date).to.exist;
+    expect(quote_expiration_date).to.exist;
+    expect(top_up_quote_id).to.equal("?");
+    expect(winston_credit_amount).to.equal("?");
   });
 });
--- src/routes/stripe/eventHandlers/paymentFailedEventHandler.ts
+++ src/routes/stripe/eventHandlers/paymentFailedEventHandler.ts
@@ -3,20 +3,18 @@ import { Stripe } from "stripe";
 import { Database } from "../../../database/database";
 import logger from "../../../logger";
 import { MetricRegistry } from "../../../metricRegistry";
-import { KoaContext } from "../../../server";
 
 export async function handlePaymentFailedEvent(
   pi: Stripe.PaymentIntent,
-  ctx: Partial<KoaContext>
+  db: Database
 ) {
   const walletAddress = pi.metadata["address"];
   logger.info(
     `🔔  Webhook received for Wallet ${walletAddress}: ${pi.status}!`
   );
   logger.info(`💸 Payment failed. ${pi.amount}`);
-  const paymentDatabase = ctx.state?.paymentDatabase as Database;
 
   // TODO: This should be the topUpQuote ID
-  await paymentDatabase.expireTopUpQuote(walletAddress);
+  await db.expireTopUpQuote(walletAddress);
   MetricRegistry.paymentFailedCounter.inc();
 }
