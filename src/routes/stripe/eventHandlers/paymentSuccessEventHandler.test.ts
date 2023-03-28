ae6bdf3f25af05e6eed984971a55c0f5a076170c
Derek Sonnenberg <derek@ardrive.io>
2023-03-28 10:51:25 -0500

    test: adjust payment success expectations PE-3031

--- src/routes/stripe/eventHandlers/paymentSuccessEventHandler.test.ts
+++ src/routes/stripe/eventHandlers/paymentSuccessEventHandler.test.ts
@@ -1,51 +1,55 @@
-import * as chai from "chai";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
+import { expect } from "chai";
 
 import { paymentIntentSucceededStub } from "../../../../tests/helpers/stubs";
-import { Database } from "../../../database/database";
-import { TopUpQuote } from "../../../database/dbTypes";
+import { tableNames } from "../../../database/dbConstants";
+import { PaymentReceiptDBResult } from "../../../database/dbTypes";
+import { PostgresDatabase } from "../../../database/postgres";
 import { handlePaymentSuccessEvent } from "./paymentSuccessEventHandler";
 
-var expect = chai.expect;
-chai.use(sinonChai);
-
-const mockPricingService = {
-  getARCForFiat: () => Promise.resolve("1.2345"),
-};
-
-// FIXME: solving merge conflict. TODO: test in integration with database
-const mockDatabase: Database = {
-  getTopUpQuote: () => Promise.resolve({} as TopUpQuote),
-  createPaymentReceipt: () => Promise.resolve(),
-} as unknown as Database;
-
-afterEach(() => {
-  sinon.restore();
-});
-
 describe("handlePaymentSuccessEvent", () => {
+  const db = new PostgresDatabase();
+
   it("should process payment and create receipt if payment quote exists", async () => {
     const paymentIntent = paymentIntentSucceededStub;
-    sinon.stub(mockDatabase, "getTopUpQuote").resolves({} as TopUpQuote);
-    sinon.stub(mockDatabase, "createPaymentReceipt").resolves();
-    sinon.stub(mockPricingService, "getARCForFiat").resolves("1.2345");
 
-    await handlePaymentSuccessEvent(paymentIntent, mockDatabase);
+    await handlePaymentSuccessEvent(paymentIntent, db);
 
-    expect(mockDatabase.getTopUpQuote).to.have.been.calledOnceWithExactly(
-      paymentIntent.metadata["address"]
-    );
-    // expect(
-    //   mockDatabase.createPaymentReceipt
-    // ).to.have.been.calledOnceWithExactly(paymentIntent.metadata["address"]);
+    const paymentReceiptDbResults = await db["knex"]<PaymentReceiptDBResult>(
+      tableNames.paymentReceipt
+    ).where({
+      payment_receipt_id: paymentIntent.id,
+    });
+    expect(paymentReceiptDbResults).to.have.length(1);
+
+    const {
+      amount,
+      currency_type,
+      destination_address,
+      destination_address_type,
+      payment_provider,
+      payment_receipt_date,
+      payment_receipt_id,
+      top_up_quote_id,
+      winston_credit_amount,
+    } = paymentReceiptDbResults[0];
+
+    // TODO: Answer ? below
+    expect(amount).to.equal(paymentIntent.amount);
+    expect(currency_type).to.equal(paymentIntent.currency);
+    expect(destination_address).to.equal("?");
+    expect(destination_address_type).to.equal("arweave");
+    expect(payment_provider).to.equal("stripe");
+    expect(payment_receipt_date).to.exist;
+    expect(payment_receipt_id).to.equal(paymentIntent.id);
+    expect(top_up_quote_id).to.equal("?");
+    expect(winston_credit_amount).to.equal("?");
   });
 
-  it("should throw an error if no payment quote is found", async () => {
-    sinon.stub(mockDatabase, "getTopUpQuote").resolves(undefined);
+  it("should throw an error if no top up quote is found", async () => {
     const paymentIntent = paymentIntentSucceededStub;
+
     try {
-      await handlePaymentSuccessEvent(paymentIntent, mockDatabase);
+      await handlePaymentSuccessEvent(paymentIntent, db);
       expect.fail("No payment quote found for 0x1234567890");
     } catch (error) {
       expect(error).to.exist;
