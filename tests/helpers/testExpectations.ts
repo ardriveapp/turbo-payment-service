3310abc067479e3d97c3f6eb29590c636779d81a
Derek Sonnenberg <derek@ardrive.io>
2023-04-12 11:35:27 -0500

    test(schema): setup new expectations PE-3458

--- tests/helpers/testExpectations.ts
+++ tests/helpers/testExpectations.ts
@@ -48,31 +48,51 @@ const expectedVarCharColumn = ({
 };
 
 export const expectedColumnInfo = {
-  user_address: expectedVarCharColumn({}),
-  user_address_type: expectedVarCharColumn({}),
-  user_creation_date: expectedDateColumn(),
-  destination_address: expectedVarCharColumn({}),
-  destination_address_type: expectedVarCharColumn({}),
-  winston_credit_balance: expectedVarCharColumn({}),
-  promotional_info: {
-    type: "jsonb",
-    maxLength: null,
-    nullable: false,
-    defaultValue: "'{}'::jsonb",
+  user: {
+    user_address: expectedVarCharColumn({}),
+    user_address_type: expectedVarCharColumn({}),
+    user_creation_date: expectedDateColumn(),
+    winston_credit_balance: expectedVarCharColumn({}),
+    promotional_info: {
+      type: "jsonb",
+      maxLength: null,
+      nullable: false,
+      defaultValue: "'{}'::jsonb",
+    },
   },
-  top_up_quote_id: expectedVarCharColumn({}),
-  amount: expectedVarCharColumn({}),
-  currency_type: expectedVarCharColumn({}),
-  winston_credit_amount: expectedVarCharColumn({}),
-  quote_expiration_date: expectedDateColumn(false),
-  quote_fulfilled_date: expectedDateColumn(),
-  quote_failed_date: expectedDateColumn(),
-  quote_creation_date: expectedDateColumn(),
-  payment_provider: expectedVarCharColumn({}),
-  payment_receipt_id: expectedVarCharColumn({}),
-  payment_receipt_date: expectedDateColumn(),
-  payment_receipt_rescinded_date: expectedDateColumn(),
-  chargeback_receipt_id: expectedVarCharColumn({}),
-  chargeback_reason: expectedVarCharColumn({}),
-  chargeback_receipt_date: expectedDateColumn(),
+
+  top_up_quote: {
+    top_up_quote_id: expectedVarCharColumn({}),
+    destination_address: expectedVarCharColumn({}),
+    destination_address_type: expectedVarCharColumn({}),
+    payment_amount: expectedVarCharColumn({}),
+    currency_type: expectedVarCharColumn({}),
+    winston_credit_amount: expectedVarCharColumn({}),
+    quote_expiration_date: expectedDateColumn(false),
+    quote_creation_date: expectedDateColumn(),
+    payment_provider: expectedVarCharColumn({}),
+  },
+
+  failed_top_up_quote: {
+    quote_creation_date: expectedDateColumn(false),
+
+    failed_reason: expectedVarCharColumn({}),
+    quote_failed_date: expectedDateColumn(),
+  },
+
+  payment_receipt: {
+    quote_creation_date: expectedDateColumn(false),
+
+    payment_receipt_id: expectedVarCharColumn({}),
+    payment_receipt_date: expectedDateColumn(),
+  },
+
+  chargeback_receipt: {
+    quote_creation_date: expectedDateColumn(false),
+    payment_receipt_date: expectedDateColumn(false),
+
+    chargeback_receipt_id: expectedVarCharColumn({}),
+    chargeback_reason: expectedVarCharColumn({}),
+    chargeback_receipt_date: expectedDateColumn(),
+  },
 };
