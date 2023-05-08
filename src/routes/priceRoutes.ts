e4f77031fd6ac438ee290142059ac0813487e9ee
dtfiedler <dylan@ar.io>
2023-05-08 13:24:06 -0600

    fix(PE-3423): fix test helpers functions

--- src/routes/priceRoutes.ts
+++ src/routes/priceRoutes.ts
@@ -5,7 +5,7 @@ import { priceBytesHandler } from "./priceBytes";
 import { priceFiatHandler } from "./priceFiat";
 
 export async function priceRoutes(ctx: KoaContext, next: Next) {
-  let currency = ctx.params.currency ?? "bytes";
+  const currency = ctx.params.currency ?? "bytes";
 
   if (currency === "bytes") {
     return priceBytesHandler(ctx, next);
