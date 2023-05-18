67238b10467eda53f652426ff1c5ab31afb73b62
Derek Sonnenberg <derek@ardrive.io>
2023-05-18 15:41:25 -0500

    feat(logs): improve price bytes route logging PE-3562

--- src/routes/priceBytes.ts
+++ src/routes/priceBytes.ts
@@ -9,29 +9,35 @@ export async function priceBytesHandler(ctx: KoaContext, next: Next) {
   const { pricingService } = ctx.state;
 
   const bytesValue = ctx.params.amount;
+
+  logger.info("GET request for price of credits for a given byte count", {
+    bytesValue,
+  });
+
   if (Number(bytesValue) > Number.MAX_SAFE_INTEGER) {
     ctx.response.status = 400;
-    ctx.message = "Byte count too large";
+    ctx.body = "Byte count too large";
+    logger.info("Byte count too large", { bytesValue });
     return next;
   }
   let bytes: ByteCount;
   try {
     bytes = ByteCount(Number(bytesValue));
   } catch (error) {
-    logger.error(error);
     ctx.response.status = 400;
     ctx.body = "Invalid byte count";
+    logger.error("Invalid byte count", { bytesValue }, error);
     return next;
   }
-  //TODO - Hit db and return 503 if unavailable
   try {
     const price = await pricingService.getWCForBytes(bytes);
     ctx.response.status = 200;
     ctx.body = price;
+    logger.info("Price found for byte count!", { price, bytesValue });
   } catch (error) {
-    logger.error(error);
     ctx.response.status = 502;
     ctx.body = "Pricing Oracle Unavailable";
+    logger.error("Pricing Oracle Unavailable", { bytesValue }, error);
   }
   return next;
 }
