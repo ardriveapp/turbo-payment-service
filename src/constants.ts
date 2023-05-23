7ab86bd05e03146bffecd3c82ece5dddd0a1a075
Derek Sonnenberg <derek@ardrive.io>
2023-05-23 15:43:24 -0500

    feat(limits): add limits for ea curr PE-3827

--- src/constants.ts
+++ src/constants.ts
@@ -21,6 +21,16 @@ export const turboFeePercentageAsADecimal = 0.2;
 // cspell:disable
 export const electronicallySuppliedServicesTaxCode = "txcd_10000000"; //cspell:disable
 
-export const minUSDPaymentAmount = 10_00; // 10.00 USD
-export const maxUSDPaymentAmount = 10_000_00; // 10,000.00 USD
-export const maxJPYPaymentAmount = 99_999_999;
+/** Min, max, and suggested payment amounts for the payment service */
+export const paymentAmountLimits = {
+  usd: { min: 10_00, max: 10_000_00, sugg: [25_00, 50_00, 100_00] },
+  brl: { min: 50_00, max: 50_000_00, sugg: [125_00, 250_00, 500_00] },
+  hkd: { min: 100_00, max: 100_000_00, sugg: [200_00, 400_00, 800_00] },
+  jpy: { min: 1_500, max: 1_500_000, sugg: [3_500, 6_500, 15_000] },
+  cad: { min: 10_00, max: 15_000_00, sugg: [25_00, 50_00, 100_00] },
+  gbp: { min: 10_00, max: 10_000_00, sugg: [20_00, 40_00, 80_00] },
+  eur: { min: 10_00, max: 10_000_00, sugg: [25_00, 50_00, 100_00] },
+  sgd: { min: 15_00, max: 15_000_00, sugg: [25_00, 75_00, 150_00] },
+  aud: { min: 15_00, max: 15_000_00, sugg: [25_00, 75_00, 150_00] },
+  inr: { min: 1000_00, max: 900_000_00, sugg: [2000_00, 4000_00, 8000_00] },
+};
