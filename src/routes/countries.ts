cbcecd3e8d59af7e6930d16df8c27958972ee99c
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2023-06-27 16:39:43 -0300

    refactor: remove try catch block

--- src/routes/countries.ts
+++ src/routes/countries.ts
@@ -4,13 +4,8 @@ import { recognizedCountries } from "../constants";
 import { KoaContext } from "../server";
 
 export async function countriesHandler(ctx: KoaContext, next: Next) {
-  try {
-    ctx.response.status = 200;
-    ctx.body = recognizedCountries;
-  } catch (error) {
-    ctx.response.status = 500;
-    ctx.body = "Internal Server Error";
-  }
+  ctx.response.status = 200;
+  ctx.body = recognizedCountries;
 
   return next;
 }
